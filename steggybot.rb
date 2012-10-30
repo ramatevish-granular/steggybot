e207dbba5ab07a6e2ec6bcded6df72a2a4136cfd
Michael Steger <mjsteger1@gmail.com>
2012-10-30 00:56:44 +0000

    Initial commit

---
+++ steggybot.rb
@@ -0,0 +1,17 @@
+require 'cinch'
+Dir.glob("plugins/*").each {|x| require_relative x}
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = "irc.freenode.net"
+    c.nick = "steggycinchbot"
+    c.channels = ["#csua", "#csuatest"]
+    #c.channels = ["#csuatest"]
+    c.plugins.plugins = [Google, UrbanDictionary, TinyURL, Cinch::Plugins::Quotes]
+    c.plugins.options[Cinch::Plugins::Quotes] = {
+      :quotes_file => "quotes.yml"
+    }
+  end
+end
+
+bot.start
