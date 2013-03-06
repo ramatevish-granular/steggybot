41fb86ba635154ef4a7cfee09187cde3a3595656
Michael Steger <mjsteger1@gmail.com>
2013-03-06 16:20:05 +0000

    Adding the plusplus feature

--- plugins/plusplus.rb
+++ plugins/plusplus.rb
@@ -3,22 +3,37 @@ require 'yaml'
 
 class PlusPlus
   include Cinch::Plugin
-
+  @help_hash = {
+    :person => "Usage: !person NAME
+Example: !person steggybot
+Returns the plusplus values from all other users about user NAME",
+    :persons_feelings => "Usage: !persons_feelings NAME
+Example !persons_feelings steggybot
+Returns the a hash of the people that that NAME has plusplus'ed, and the number of times."
+  }
+  match /karma (.*)/i, method: :lookup
   listen_to :channel
-  match /persons_feelings (\w+)/i, method: :lookup_feelings
-  match /person (\w+)/i, method: :lookup
+  match /persons_feelings (.*)/i, method: :lookup_feelings
+  match /person (.*)/i, method: :lookup
 
   def lookup(m, name)
     values = plusplus_file
-    values = values.keys.map { |key| values[key] if values[key][name] > 0 }.compact
-    p "values: #{values}"
+    # Don't want to always nameping people, so we mangle the name a little
+    values = values.keys.map { |key| {mangle_string(key) => values[key][name]} if values[key][name] }.compact
     result = values.empty? ? "no results" : values
+    result = "#{name}'s karma: " + result.join(", ")
     m.reply(result)
+  end
+
+  def mangle_string(string)
+    string.chop + "." + string[-1]
   end
 
   def lookup_feelings(m, name)
     values = plusplus_file
-    result = (values.keys.include? name) ? values[name] : "no results"
+    base_hash = (values.keys.include? name) ? values[name] : "no results"
+    result = base_hash.keys.map {|key| [mangle_string(key), base_hash[key]] }
+    result = Hash[result]
     m.reply(result)
   end
 
@@ -28,8 +43,10 @@ class PlusPlus
   end
 
   def listen(m)
-    plusplus = %r{(?<to>(\w+))\+\+}.match m.message
-    add(m.user.nick, plusplus[:to]) if plusplus
+    plusplus = %r{(?<to>(.*))\+\+$}.match m.message
+    minusminus  = %r{(?<to>(.*))\-\-$}.match m.message
+    change(m.user.nick, plusplus[:to], :+) if plusplus
+    change(m.user.nick, minusminus[:to], :-) if minusminus
   end
 
   def plusplus_file()
@@ -39,17 +56,15 @@ class PlusPlus
     values
   end
 
-  def add(from, to)
-    p "from is #{from}, to is #{to}"
+  def change(from, to, function)
     values = plusplus_file || {}
-    p "values is #{values}"
     froms_feelings = (values.keys.include? from) ? values[from] : Hash.new { |hash, key| hash[key] = 0 }
-    p "now is is #{froms_feelings}"
-    froms_feelings[to] += 1
-    p "now is is #{froms_feelings}"
+    p froms_feelings
+    p to
+    froms_feelings[to] = 0 if froms_feelings[to].nil?
+    froms_feelings[to] = froms_feelings[to].send(function, 1)
+    froms_feelings.delete(to) if froms_feelings[to] == 0
     values[from] = froms_feelings
-    p "now is is #{froms_feelings}"
     File.open(@plusplus_file, 'w') { |f| f.write (YAML.dump values) }
   end
 end
-
