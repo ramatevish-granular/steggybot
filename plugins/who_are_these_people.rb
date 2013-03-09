b2fe8cd0959b290c1f630fc7e01ba57ef76bc88e
Nathan Fair <nfair@saymedia.com>
2013-03-08 16:24:58 -0800

    addwho and removewho regex for user name was too restrictive
    now allow "-" in user name

--- plugins/who_are_these_people.rb
+++ plugins/who_are_these_people.rb
@@ -4,18 +4,18 @@ class WhoAreThesePeople
   include Cinch::Plugin
 
   listen_to :channel
-  match /addwho (\w+) (.+)/i, method: :add
-  match /removewho (\w+)/i, method: :remove
-  
+  match /addwho ([\w-]+) (.+)/i, method: :add
+  match /removewho ([\w-]+)/i, method: :remove
+
   def initialize(*args)
     super
     @identities = config[:identities]
   end
-  
+
   def add (m, username, real_name)
     # add it to the list
     existing_ids = get_identities || {}
-    
+
     if existing_ids[username.downcase] && (existing_ids[username.downcase][:added_by] == username.downcase)
       if m.user.nick.downcase == username.downcase
         existing_ids[username.downcase] = {:name => real_name, :added_by => m.user.nick.downcase, :updated_at => DateTime.now}
@@ -26,7 +26,7 @@ class WhoAreThesePeople
     else
       existing_ids[username.downcase] = {:name => real_name, :added_by => m.user.nick.downcase, :updated_at => DateTime.now}
     end
-  
+
     # write it to the file
     output = File.new(@identities, 'w')
     output.puts YAML.dump(existing_ids)
@@ -34,28 +34,28 @@ class WhoAreThesePeople
 
     m.reply "#{m.user.nick}: #{username} successfully added as '#{real_name}'."
   end
-  
+
   def remove(m, username)
     existing_ids = get_identities || {}
-    
+
     unless existing_ids[username]
       m.reply "#{m.user.nick}: #{username} not in identities list."
       return
     end
-    
+
     if (m.user.nick.downcase == username.downcase) || (m.user.nick.downcase == existing_ids[username][:added_by])
       existing_ids.delete(username)
       # write it to the file
       output = File.new(@identities, 'w')
       output.puts YAML.dump(existing_ids)
       output.close
-    
+
       m.reply "#{m.user.nick}: #{username} removed from identities list."
     else
       m.reply "#{m.user.nick}: Only the identity creator or subject can remove an identity from the list."
     end
   end
-  
+
   def listen(m)
     match = /who is (\w+)/i.match(m.message)
     if match
@@ -65,7 +65,7 @@ class WhoAreThesePeople
       end
     end
   end
-  
+
   protected
 
   def get_identities
