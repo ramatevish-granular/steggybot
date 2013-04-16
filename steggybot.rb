936b6ed819f7c2092f65c690a14a43835e0d8dc3
Greg <elorian.mestec@gmail.com>
2013-04-16 15:18:42 -0700

    Switched to using #spacecake as channel and spacecakebot as nick

--- steggybot.rb
+++ steggybot.rb
@@ -5,10 +5,10 @@ Dir.glob("plugins/*/*.rb").each {|x| require_relative x}
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.freenode.net"
-    c.nick = "steggybot"
-    c.channels = ["#csua", "#csuatest", "##csua"]
+    c.nick = "spacecakebot"
+    c.channels = ["#spacecake"]
     c.plugins.plugins = [Google, UrbanDictionary, TitleGrabber, Quotes, Pokedex, Youtube, 
-                         YaBish, Roll, WhoAreThesePeople, PlusPlus, Pazudora]
+                         YaBish, Roll, WhoAreThesePeople, PlusPlus, Pazudora, Test]
     c.plugins.plugins << Help
     c.plugins.options[Quotes] = {
       :quotes_file => "db/quotes.yml"
