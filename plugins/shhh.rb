7dcdfaeac4a93706d25c1b3dc84220c5b74bbcde
Chris Zhang <christinejingzhang@gmail.com>
2017-08-30 19:22:11 -0700

    Quiet time module to make steggybot de-dupe itself, and also allow force-quitting from people

---
+++ plugins/shhh.rb
@@ -0,0 +1,38 @@
+require 'cinch'
+
+class Shhh
+  include Cinch::Plugin
+
+  authorized_users = ["czhangolin", "steggy", "wsong"]
+
+  match /quiet/, method: :be_quiet
+
+  match /quiet --f (.+)/, method: :force_quit
+  @help_hash = {
+    :mute => "Usage: !quiet
+Example: !quiet
+Leaves if there's a name conflict or certain people ask"
+  }
+
+  def be_quiet(m)
+    username_to_check = bot.nick.gsub("_", "")
+
+    while username_to_check.length < bot.nick.length
+      if m.channel.has_user?(username_to_check)
+        m.reply("FINE, BE THAT WAY")
+        bot.quit("its meeeeee, im the copy, nooooooo")
+      end
+      username_to_check += "_"
+    end
+  end
+
+  def force_quit(m, username)
+    return unless username == bot.nick.downcase
+
+    if m.channel.opped?(m.user) || authorized_users.include?(m.user.nick.downcase)
+      bot.quit("bye")
+    else
+      m.reply("you are not on the sudoers list. this attempt has been reported.")
+    end
+  end
+end
